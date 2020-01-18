9e89bf357add8d6bab6fcf27025388ed0a6ce60b
Noel Gomez <nollgmz4@gmail.com>
2020-01-18 07:55:29 -0800

    Created navigation demo and recipe window

--- app_windows/HealthHome.jsx
+++ app_windows/HealthHome.jsx
@@ -1,12 +1,16 @@
 import React, { Component } from 'react';
-import { View, Button, StyleSheet, Text } from "react-native";
+import { View, Button, StyleSheet, TextInput, Dimensions } from "react-native";
 import { registerRootComponent } from "expo";
 import MongoDB from "../app_modules/MongoDB";
+import { createAppContainer } from 'react-navigation';
+import { createStackNavigator } from 'react-navigation-stack';
+import Recipe from "./Recipe";
 class HealthHome extends Component {
     constructor(props) {
         super(props);
         this.state = {
-            loggedIn: false
+            loggedIn: false,
+            calorieGoal: null
         };
     }
 
@@ -14,10 +18,20 @@ class HealthHome extends Component {
         MongoDB.loadClient().then(() => this.setState({ loggedIn: true }))
     }
 
+    onChangeText = text => {
+        console.log(text);
 
+    }
 
     render() {
-        
+        return (<View style={styles}>
+            <TextInput
+                style={{ margin:Dimensions.get('window').height/2,height: 40,width:Dimensions.get('window').width/2, borderColor: 'gray', borderWidth: 1,alignSelf:'center' }}
+                onChangeText={this.onChangeText}
+                onSubmitEditing={()=>this.props.navigation.navigate("Recipes")}
+                keyboardType="number-pad"
+            />
+        </View>);
     }
 }
 
@@ -30,4 +44,14 @@ const styles = StyleSheet.create({
         justifyContent: 'center',
     },
 });
-export default registerRootComponent(HealthHome);
+
+const MainNavigator = createStackNavigator({
+    Home: HealthHome,
+    Recipe: Recipe
+}, {
+    initialRouteName: 'Home',
+    defaultNavigationOptions: { headerShown: false }
+});
+
+const App = createAppContainer(MainNavigator);
+export default registerRootComponent(App);
---
+++ app_windows/Recipe.jsx
@@ -0,0 +1,17 @@
+import React, { Component } from 'react';
+import { View, Text } from 'react-native';
+
+class Recipe extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {};
+    }
+
+    render() {
+        return (
+            <View><Text>RECIPES</Text></View>
+        );
+    }
+}
+
+export default Recipe;
